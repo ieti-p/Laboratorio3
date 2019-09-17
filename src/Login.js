03eb114e2cc1a9f0d35218a06c6752467aaa974c
Cesar Eduardo Lanos Camacho <clanos@elliptical.co>
2019-09-17 17:16:19 -0500

    PROGRESO 2DO PUNTO

---
+++ src/Login.js
@@ -0,0 +1,47 @@
+import React from 'react';
+//import TextField from 'material-ui/core/TextField';
+import {render} from 'react-dom';
+
+import './login.css';
+
+//
+
+import deburr from 'lodash/deburr';
+//import Autosuggest from 'react-autosuggest';
+//import match from 'autosuggest-highlight/match';
+//import parse from 'autosuggest-highlight/parse';
+//import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import MenuItem from '@material-ui/core/MenuItem';
+import Popper from '@material-ui/core/Popper';
+import { makeStyles } from '@material-ui/core/styles';
+//
+
+
+//
+
+
+
+//
+
+function Login() {
+    //const title1=<h1>Task Planner</h1>;
+    return (
+      <div className="login"  text-justify="inter-word" >         
+        <header className="login-header" >
+          <h1>Task Planner</h1>
+          <h2>Ingrese los datos de inicio</h2>
+        </header>   
+
+      </div>
+
+
+
+      );
+
+}
+
+
+export default Login;
+
+

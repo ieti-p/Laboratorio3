5b4bb72c350864dbe8f215108aa8a196458010ae
cesareduardo824 <cesareduardo824@gmail.com>
2019-09-18 01:00:24 -0500

    2do terminado

--- src/Login.js
+++ src/Login.js
@@ -1,47 +1,73 @@
 import React from 'react';
-//import TextField from 'material-ui/core/TextField';
-import {render} from 'react-dom';
-
-import './login.css';
-
-//
-
 import deburr from 'lodash/deburr';
-//import Autosuggest from 'react-autosuggest';
-//import match from 'autosuggest-highlight/match';
-//import parse from 'autosuggest-highlight/parse';
-//import TextField from '@material-ui/core/TextField';
+import Autosuggest from 'react-autosuggest';
+import match from 'autosuggest-highlight/match';
+import parse from 'autosuggest-highlight/parse';
+import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
 import Popper from '@material-ui/core/Popper';
 import { makeStyles } from '@material-ui/core/styles';
-//
+import Button from '@material-ui/core/Button';
+import './login.css';
+//npm install autosuggest-highlight --save
+const useStyles = makeStyles(theme => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    display: 'none',
+  },
+}));
 
 
-//
+export default function Login() {
 
+  const classes = useStyles();
+  return (
 
+    <div className="login" text-justify="inter-word" >
 
-//
+      <header className="login-header" >
 
-function Login() {
-    //const title1=<h1>Task Planner</h1>;
-    return (
-      <div className="login"  text-justify="inter-word" >         
-        <header className="login-header" >
-          <h1>Task Planner</h1>
-          <h2>Ingrese los datos de inicio</h2>
-        </header>   
+        <h1>Task Planner</h1>
 
-      </div>
+        <h2>Ingrese los datos de inicio</h2>
+        <TextField
+          id="outlined-email-input"
+          label="Email"
+          className={"login"}
+          type="email"
+          name="email"
+          autoComplete="email"
+          margin="normal"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-password-input"
+          label="Password"
+          className={"login"}
+          type="password"
+          autoComplete="current-password"
+          margin="normal"
+          variant="outlined"
 
+        />
+        <div>
+          <Button variant="outlined" color="secondary" className={classes.button}>
+            Loggin
+      </Button>
+          <Button variant="outlined" color="secondary" className={classes.button}>
+            Create Acount
+      </Button>
+        </div>
+      </header>
 
+    </div>
 
-      );
+  );
 
 }
 
 
-export default Login;
-
-
+//export default Login;
